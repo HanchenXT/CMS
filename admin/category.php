20da959f5cfe4d194e8f36dd930b6dfcc74700fe
HanchenXT <ruochenhan@gmail.com>
2018-05-03 12:49:41 -0600

    section 13

--- admin/category.php
+++ admin/category.php
@@ -26,6 +26,10 @@ include "includes/header.php";
                         <div class="col-xs-4">
                             
                             <form action="" method="post">
+                            <?php 
+                            add_cate();
+                            delete_cate();
+                            ?>
                             <div class="form-group">
                                <label for="cat-title">Add Category</label>
                                 <input type="text" class="form-control" name="cat_title" required>
@@ -34,31 +38,7 @@ include "includes/header.php";
                                 <input class="btn btn-primary" type="submit" name="add_cate" value="Add Category">
                             </div>
                             </form>
-                            
-                            <form action="" method="post">
-                            <div class="form-group">
-                               <label for="cat-title">Update Category</label>
-                                <?php
-                                // edit category
-                                   if(isset($_GET['edit'])) {
-                                       $cat_id = $_GET['edit'];
-                                        $query = "SELECT * FROM categories WHERE cat_id = $cat_id ";
-                                        $query_edit = mysqli_query($connection, $query);
-                                        while($row = mysqli_fetch_assoc($query_edit)) {
-                                           $cat_id = $row['cat_id'];
-                                           $cat_title = $row['cat_title'];
-                                           ?>
-                                           <input value="<?php if (isset($cat_title)) {echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
-                                           <?php
-                                        }
-
-                                   }
-                                ?>
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="update_cate" value="Update Category">
-                            </div>
-                            </form>
+                        <?php include "includes/update_cate.php";?>
                         </div>
                         
                         <div class="col-xs-2">
@@ -72,39 +52,20 @@ include "includes/header.php";
                                 </thead>
                                 <tbody>
                                   
-                                   <?php
-                                   // add category
-                                   if(isset($_POST['add_cate'])) {
-                                        $cat_title = $_POST['cat_title'];
-                                        $query = "INSERT INTO categories(cat_title) VALUE ('{$cat_title}') ";
-                                        $insert_cate = mysqli_query($connection, $query);
-
-                                        if (!$insert_cate) {
-                                            die('insert failed' . mysqli_error($connection));
-                                        }
-
-                                   }
-
-                                   $query = "SELECT * FROM categories";
-                                   $query_cate = mysqli_query($connection, $query);
-                                   while($row = mysqli_fetch_assoc($query_cate)) {
-                                       $cat_id = $row['cat_id'];
-                                       $cat_title = $row['cat_title'];
-                                       echo "<tr>";
-                                       echo "<td>{$cat_id}</td>";
-                                       echo "<td>{$cat_title}</td>";
-                                       echo "<td><a href='category.php?delete={$cat_id}'>Delete</td>";
-                                       echo "<td><a href='category.php?edit={$cat_id}'>Edit</td>";
-                                       echo "</tr>";
-                                   }
-
-                                   if (isset($_GET['delete'])) {
-                                       $the_cat_id = $_GET['delete'];
-                                       $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
-                                       $delete_query = mysqli_query($connection, $query);
-                                       header("Location: category.php");
-                                   }
-                                   ?>
+                                <?php
+                                $query = "SELECT * FROM categories";
+                                $query_cate = mysqli_query($connection, $query);
+                                while($row = mysqli_fetch_assoc($query_cate)) {
+                                   $cat_id = $row['cat_id'];
+                                   $cat_title = $row['cat_title'];
+                                   echo "<tr>";
+                                   echo "<td>{$cat_id}</td>";
+                                   echo "<td>{$cat_title}</td>";
+                                   echo "<td><a href='category.php?delete={$cat_id}'>Delete</td>";
+                                   echo "<td><a href='category.php?edit={$cat_id}'>Edit</td>";
+                                   echo "</tr>";
+                                }
+                                ?>
                                 
                                 </tbody>
                             </table>
