a290930c7ac388c99ec1d9b5cafd2b340c914c63
HanchenXT <ruochenhan@gmail.com>
2018-05-06 17:46:54 -0600

    fix

--- category.php
+++ category.php
@@ -24,52 +24,52 @@ include "includes/header.php";
                 <?php
                 if (isset($_GET['category'])) {
                     $category_id = $_GET['category'];
-                }
-                $query = "SELECT * FROM posts WHERE post_category_id = {$category_id}";
-                $query_posts = mysqli_query($connection, $query);
-                while($row = mysqli_fetch_assoc($query_posts)) {
-                    $post_id = $row['post_id'];
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = substr($row['post_content'], 0, 100);
-                    $post_tags = $row['post_tags'];
+                    $query = "SELECT * FROM posts WHERE post_category_id = {$category_id}";
+                    $query_posts = mysqli_query($connection, $query);
+                    while($row = mysqli_fetch_assoc($query_posts)) {
+                        $post_id = $row['post_id'];
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = substr($row['post_content'], 0, 100);
+                        $post_tags = $row['post_tags'];
 
-                    ?>
+                        ?>
 
-                    <h1 class="page-header">
-                        Page Heading
-                        <small>Secondary Text</small>
-                    </h1>
+                        <h1 class="page-header">
+                            Page Heading
+                            <small>Secondary Text</small>
+                        </h1>
 
-                    <!-- First Blog Post -->
-                    <h2>
-                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php echo $post_author ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
-                    <hr>
-                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                    <hr>
-                    <p><?php echo $post_content ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+                        <hr>
+                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                        <hr>
+                        <p><?php echo $post_content ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                    <hr>
+                        <hr>
 
-                    <!-- Pager -->
-                    <ul class="pager">
-                        <li class="previous">
-                            <a href="#">&larr; Older</a>
-                        </li>
-                        <li class="next">
-                            <a href="#">Newer &rarr;</a>
-                        </li>
-                    </ul>
+                        <!-- Pager -->
+                        <ul class="pager">
+                            <li class="previous">
+                                <a href="#">&larr; Older</a>
+                            </li>
+                            <li class="next">
+                                <a href="#">Newer &rarr;</a>
+                            </li>
+                        </ul>
 
-                <?php
+                    <?php
+                    }
                 }
                 ?>    
             </div>
