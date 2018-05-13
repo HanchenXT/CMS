529afc26f64cf8ec5b6154b571480772c0ec833b
HanchenXT <ruochenhan@gmail.com>
2018-05-12 23:30:09 -0600

    login

--- includes/siderbar.php
+++ includes/siderbar.php
@@ -16,13 +16,49 @@
         <!-- /.input-group -->
     </div>
     
+    <!--Login -->
+    <?php
+    if(ifItIsMethod('post')){
+        if(isset($_POST['login'])){
+            if(isset($_POST['username']) && isset($_POST['password'])){
+                login_user($_POST['username'], $_POST['password']);
+            }else {
+                redirect('index');
+            }
+        }
+    }
+    ?>
+    <div class="well">
+        <?php if(isset($_SESSION['user_role'])): ?>
+             <h4>Logged in as <?php echo $_SESSION['username'] ?></h4>
+             <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+        <?php else: ?>
+             <h4>Login</h4>
+                    <form method="post">
+                    <div class="form-group">
+                        <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                    </div>
+
+                    <div class="input-group">
+                        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                        <span class="input-group-btn">
+                           <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                        </span>
+                    </div>
+
+                    <div class="form-group">
+                        <a href="forgot.php?forgot=<?php echo uniqid(true); ?>">Forgot Password</a>
+                    </div>
+                </form>
+                <!-- /.input-group -->
+        <?php endif; ?>  
+    </div>
+
+    <!-- Blog Categories Well -->
     <?php
     $query = "SELECT * FROM categories";
     $query_cate = mysqli_query($connection, $query);
-    
     ?>
-
-    <!-- Blog Categories Well -->
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
@@ -51,5 +87,4 @@
     <?php
     include "widget.php";
     ?>
-
-</div>
+</div>            
